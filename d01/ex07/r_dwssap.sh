908cdd4c2b8434e495387b544ee7bcc7c858a32c
sshiling <s.shilingov@gmail.com>
2018-04-20 21:33:03 +0300

    d01 c_pool

---
+++ d01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /etc/passwd | grep -v '^#' | sed '1d; n; d' | cut -d ':' -f 1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ' ' | sed 's/ /, /g' | rev | sed 's/ ,/./' | rev | tr -d '\n'
---
+++ d01/ex08/bon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ldapsearch  -Q sn="*bon*" | grep "sn: " | wc -l | sed 's/ //g' 
